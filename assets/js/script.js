7dae0c1400e44c33f5be2ee397e70f55f9199dd4
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-03-06 15:59:30 -0800

    remove global script and asyncairtable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,3 @@
-import './asyncAirtable-beta.min.js';
-const asyncAirtable = new AsyncAirtable("keyyCSADD61J0D4wQ", "appGzEQkD3QySIDvj", {baseURL: "https://api.vacfind.org/v0"});//, { ...CONFIG }
-
-
-export var loadLinks = async (filterOpts) => {
-
-	// let allrecords = []
-
-	return asyncAirtable.select('links', {
-		sort: [
-			{ field: 'group', direction: 'asc' }
-		],
-		where: filterOpts
-	}).then( (results) => persistToStorage(results))
-};
-
-const persistToStorage = (results) => {
-	console.log("persisting to storage")
-	if (results) {
-		localStorage.setItem("vacFind-links", JSON.stringify(results));
-	}
-	return results
-}
-
-export const retrieveFromStorage = () => {
-	console.log("retrieving from storage")
-	return JSON.parse(localStorage.getItem("vacFind-links"))
-}
-
-
-export const isOfficialResource = (record) => {
-	return record.fields.verification_status == "government resource" || record.fields.source_type == "government";
-}
 
 /// take an array of keys and an airtable record and check for a value at each key and returning the first one found
 /// needs testing
